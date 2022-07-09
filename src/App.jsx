e56cf29aef4b081e7f9203fe4ef863f7b3236f6b
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-07-09 23:23:52 +0300

    add path route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'redux/auth';
+import { Routes, Route } from 'react-router-dom';
+
 import Container from './components/Container';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomePage from 'pages/HomePage';
+import RegisterPage from 'pages/RegisterPage';
+import LoginPage from 'pages/LoginPage';
+import ContactsPage from 'pages/ContactsPage';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
   return (
     <Container>
       <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+      </Routes>
 
       <Footer />
     </Container>

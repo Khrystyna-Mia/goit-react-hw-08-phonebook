126b77db3dd8df925be49c6a2bb15862c2d80f9c
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-07-10 01:27:06 +0300

    fix

--- src/redux/auth/auth-selectors.jsx
+++ src/redux/auth/auth-selectors.jsx
@@ -2,7 +2,7 @@ const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUserEmail = state => state.auth.user.email;
 
-const getIsFetchingCurrent = state => state.auth.isFFetchingCurrentUser;
+const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
 
 const authSelectors = {
   getIsLoggedIn,
